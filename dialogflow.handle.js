f512d6d71e255198c9309ff925ad56fc471c9541
hiepnv <nguyenhiep96vn@gmail.com>
2019-03-15 11:20:45 +0700

    First Commit

---
+++ dialogflow.handle.js
@@ -0,0 +1,35 @@
+var dialogflow = require('dialogflow');
+//import dialogflow from 'dialogflow';
+
+class DialogflowHandle {
+  constructor () {
+    this.query = '';
+    this.sessionClient = new dialogflow.SessionsClient();
+    this.sessionPath =
+      this.sessionClient.sessionPath(
+        process.env.PROJECT_DIALOGFLOW_ID,
+        process.env.DIALOGFLOW_SESSION_ID
+      );
+    this.request = {
+      session: this.sessionPath,
+      queryInput: {
+        text: {
+          text: '',
+          languageCode: 'en-US',
+        },
+      },
+    };
+  }
+
+  handleMessage (sentence) {
+    this.request.queryInput.text.text = sentence
+    return new Promise(
+      (resolve, reject) => {
+        this.sessionClient.detectIntent(this.request)
+          .then(resolve)
+          .catch(reject);
+      }
+    )
+  }
+}
+module.exports = DialogflowHandle;
---
+++ server.js
@@ -0,0 +1,45 @@
+if (process.env.NODE_ENV !== 'production') { // Đọc biến môi trường trong file env ở local
+  var env = require('node-env-file');
+  env('.env');
+}
+
+var fbApi = require('facebook-chat-api');
+var fs = require('fs');
+var DialogflowHandle = require('./dialogflow.handle');
+// import fbApi from 'facebook-chat-api';
+// import fs from 'fs';
+// import DialogflowHandle from './dialogflow.handle';
+
+const readFileSestion = () => { // Hàm này để đọc file sessions đăng nhập của facebook
+  try{
+    const file = fs.readFileSync('appstate.json', 'utf8');
+    return JSON.parse(file);
+  } catch(error) {
+    return null;
+  }
+};
+
+const appState = readFileSestion();
+const credientials = { // Tạo biến lưu sesstions, email và mật khẩu fb. (Nêu như đã có sessions và sessions vẫn còn hạn thì không đăng nhập lại)
+  appState,
+  email: process.env.FB_EMAIL,
+  password: process.env.FB_PASSWORD
+};
+const dialogflowHandle = new DialogflowHandle();
+
+fbApi(credientials, (err, api) => {
+  if(err) return console.error(err);
+  fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState())); // Ghi lại sessions mới sau khi đã refresh
+
+  api.listen((err, message) => {
+    dialogflowHandle.handleMessage(message.body)
+      .then((data) => {
+        const result = data[0].queryResult;
+        api.sendMessage(result.fulfillmentText, message.threadID);
+      })
+      .catch((error) => {
+        //api.sendMessage(`Opps! Error`,message.threadID);
+          api.sendMessage(`Error: ${error}`, message.threadID);
+      });
+  });
+});
